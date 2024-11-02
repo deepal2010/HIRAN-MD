c2763d0b9f4ebcb6f3d431cabcc4214af963c716
deepal2010 <ddeepali373@gmail.com>
2024-11-02 18:49:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qctgkCLT#hDM_0mJgGAeqYVjxuf_V3FJIkmtxSXGXQdfufa-E4Q4",
+SESSION_ID: process.env.SESSION_ID || "029RhRgR#5CwH9FCGlgqne1D1-31bkK8fM3vwTOV26NnyiwoxgdM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/187024208?v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello i am alive now",
 };
